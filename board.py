6bbc2a8a17a970294a37b8a50f917ee69765439f
Jakub Molinski <jakub@molinski.dev>
2020-03-31 00:41:19 +0200

    Add python implementation

---
+++ board.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from typing import Dict, List
+
+from group_areas import Coords, ListOfAreas, group_areas_by_player
+
+
+class Board:
+    board: List[List[int]]
+
+    FREE_FIELD = -1
+
+    def __init__(self, width: int, height: int) -> None:
+        if width * height > 1000 * 1000:
+            raise ValueError("max supported board size is 1,000,000 fields")
+
+        self.width = width
+        self.height = height
+        self.board = [[-1] * width for _ in range(height)]
+
+    def print(self) -> str:
+        def row_to_string(row: List[int]) -> str:
+            return "".join(["." if c == -1 else str(c) for c in row]) + "\n"
+
+        return "".join(reversed(list(row_to_string(row) for row in self.board)))
+
+    def get_grouped_areas(self) -> Dict[int, ListOfAreas]:
+        return group_areas_by_player(self.board)
