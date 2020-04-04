595c99d36c932db24a0594cfe8da17e00ef1082b
Jakub Molinski <jakub@molinski.dev>
2020-04-04 20:17:08 +0200

    Add test_check_busy_fields

--- src/test_scenarios.py
+++ src/test_scenarios.py
@@ -2,7 +2,7 @@ import random
 
 from typing import Any, Dict
 
-from part1 import gamma_move
+from part1 import gamma_busy_fields, gamma_move
 from test_tools import (
     EMPTY_LINE,
     ScenarioType,
@@ -22,7 +22,7 @@ def fill_board_with_collisions(store: StatementStoreType, **kwargs: Any) -> None
     player = 2
 
     for _ in range(35):
-        x, y = random.randint(0, 10), random.randint(0, 10)
+        x, y = random.randint(0, 9), random.randint(0, 9)
         store(assert_call(gamma_move, board, player, x, y))
 
     delete_board(store, board)
@@ -44,9 +44,25 @@ def fill_board_without_collisions(store: StatementStoreType, **kwargs: Any) -> N
     delete_board(store, board)
 
 
+def test_check_busy_fields(store: StatementStoreType, **kwargs: Any) -> None:
+    doc = "busy_fields, with collisions, many players, unlimited areas"
+    store(make_comment(doc))
+
+    board = make_board(store, 7, 13, 3, 100)
+
+    for _ in range(35):
+        x, y = random.randint(0, 6), random.randint(0, 12)
+        player = random.randint(1, 3)
+        store(assert_call(gamma_move, board, player, x, y))
+        store(assert_call(gamma_busy_fields, board, player))
+
+    delete_board(store, board)
+
+
 scenarios: Dict[str, ScenarioType] = {
     "fill_board_with_collisions": fill_board_with_collisions,
     "fill_board_without_collisions": fill_board_without_collisions,
+    "test_check_busy_fields": test_check_busy_fields,
 }
 
 
